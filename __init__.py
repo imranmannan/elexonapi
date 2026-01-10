4b1806c6e202d8d4bb2290fea5edd1a50d4a757a
Imran <imran.a.mannan@gmail.com>
2026-01-10 21:15:45 +0100

    add help method

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 
-from .datasets import datasets, browse
+from .datasets import datasets, browse, help
 from .download import download
 print("elexonapi: Remember, you need to pass '_from' instead of 'from'!")
--- datasets.py
+++ datasets.py
@@ -6,7 +6,7 @@ import itables
 DEFAULT_SPEC = Path(__file__).parent / "prod-insol-insights-api.json"
 
 def get_datasets(openapi_path=None):
-    path = openapi_path or DEFAULT_SPEC                  
+    path = openapi_path or DEFAULT_SPEC
     return  pd.DataFrame(build_registry(path))
 
 datasets = get_datasets()
@@ -22,3 +22,9 @@ def get_operation_from_alias(alias, operation_aliases=operation_aliases):
 
 def browse(datasets=datasets):
     return itables.show(datasets, classes = 'display compact',columnDefs=[{"className": "dt-left", "targets": "_all"}])
+
+def help(alias, datasets=datasets):
+    operation = get_operation_from_alias(alias)
+    ds = datasets[datasets["operation"] == operation].iloc[0].to_dict()
+    print(ds['description'])
+    return ds
