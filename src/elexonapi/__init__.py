1e1cce2c7c940b9c3185ef626de1d33fc80373fe
Imran <iman@equinor.com>
2026-01-13 15:35:24 +0100

    remove warning about from parameter

--- src/elexonapi/__init__.py
+++ src/elexonapi/__init__.py
@@ -8,13 +8,13 @@ from __future__ import annotations
 from .datasets import datasets, browse, help
 from .download import ElexonClient
 
-import warnings
-
-_warn_msg = (
-    "elexonapi: Use `_from` instead of the Python reserved word "
-    "`from` when supplying date ranges."
-)
-warnings.warn(_warn_msg, stacklevel=2)
+# warning turned off for now
+# import warnings
+# _warn_msg = (
+#     "elexonapi: Use `_from` instead of the Python reserved word "
+#     "`from` when supplying date ranges."
+# )
+# warnings.warn(_warn_msg, stacklevel=2)
 
 __version__ = "0.1.3"
 __all__ = ["datasets", "browse", "help", "ElexonClient", "__version__"]
